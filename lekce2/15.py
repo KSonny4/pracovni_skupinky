d7c89c4ec611a5c2a72940d3504b4915ce77f3d8
Petr Kubelka <petr.kubelka@avast.com>
2020-03-31 17:37:49 +0200

    delete duplicate task 15

--- lekce2/15.py
+++ lekce2/15.py
@@ -1,6 +1,5 @@
 import sys
 
-
 # Predpoklada male pismeno na zacatku -- sys.argv[1] == "velbloudHoniHada".
 camel = sys.argv[1]
 upper_indices = [0] + [i for i in range(len(camel)) if camel[i].isupper()] + [len(camel)]
@@ -8,24 +7,17 @@ snake = "_".join([camel[upper_indices[i]:upper_indices[i + 1]]for i in range(len
 print(snake)
 
 
-word = sys.argv[1]
-pos = [i for i, e in enumerate (word+'A') if e.isupper()]
-parts = [word[pos[j]:pos[j+1]] for j in range(len(pos)-1)]
+pos = [i for i, e in enumerate (camel+'A') if e.isupper()]
+parts = [camel[pos[j]:pos[j+1]] for j in range(len(pos)-1)]
 answer = ('_'.join(parts)).lower()
 print(answer)
-
-
-word = sys.argv[1]
-upper_indices = [0] + [i for i in range(len(word)) if word[i].isupper()] + [len(word)]
-print(upper_indices)
-snake = "_".join([word[upper_indices[i]:upper_indices[i + 1]].lower() for i in range(len(upper_indices) - 1)]).lower()
-print(snake)
 
 
 s = 'velbloudHoniHada'
 print([c for c in s])
 result = "".join(["_" +c.lower() if c.isupper() else c for c in s])
 print(result)
+
 
 s = 'velbloudHoniHada'
 print([c for c in s])
