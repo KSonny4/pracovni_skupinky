285f300b772ff85c2cdc219e132da013ece1408a
Petr Kubelka <petr.kubelka@avast.com>
2020-04-14 21:47:09 +0200

    set comment

--- live/lekce4/14.py
+++ live/lekce4/14.py
@@ -11,6 +11,7 @@ print(f" 2. nejvetsi prvek {serazeny[-2]}")
 seznam_set = sorted(seznam)
 
 # udelame z toho set --> jako distinct v sql
+# set nejde indexovat, musime ho prevest na list
 seznam_set = list(set(seznam_set))
 print(f"seznam : {seznam_set}")
 print(f"SET!! 2 nejvetsi max: {seznam_set[-2]}")
